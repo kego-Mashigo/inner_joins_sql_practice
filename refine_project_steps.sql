4cfe0f068e2ab0362d390494bbfdae95b8201405
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-09-03 19:52:58 +0200

    Revert "Delete refine project steps script."
    This reverts commit cf1e3cd1dfe5e8997c0fd5c2845c5ca00c3100df.

---
+++ refine_project_steps.sql
@@ -0,0 +1,12 @@
+/*
+===========================================================
+TO-DO: REFINE PROJECT
+===========================================================
+
+1. Create a clean and structured README.
+2. Review and answer all business questions.
+3. Refine and clean the SQL scripts.
+4. Create and add a professional GitHub banner.
+7. Review the full project structure, and documentation.
+===========================================================
+*/
